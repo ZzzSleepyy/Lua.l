60a961bf6172b5217f0b7afc8f796b9b9526810c
Niqouu <100042636+ZzzSleepyy@users.noreply.github.com>
2024-01-07 18:11:00 +0800

    Death Script.lua

---
+++ Death Script.lua
@@ -0,0 +1,21 @@
+
+
+local Player = game.Players.LocalPlayer
+local Char = Player.Character
+
+local tweenInfo = TweenInfo.new(2, Enum.EasingStyle.Linear, Enum.EasingDirection.InOut)
+
+
+
+local function playJoinSound()
+	local sound = Instance.new("Sound")
+	sound.SoundId = "rbxassetid://15546405086"
+	sound.Parent = game:GetService("SoundService")
+	sound:Play()
+end
+
+
+Char:WaitForChild("Humanoid").Died:Connect(function()
+	playJoinSound()
+	script.Parent.Enabled = true
+end)
